717bde704fe355e11464d4a188c17e3f5585d738
Beatriz Rodrigues de Sousa <89947825+biatrz@users.noreply.github.com>
2021-09-04 01:51:54 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a;
+  printf("Digite um número: \n");
+  scanf("%d",& a);
+  if(a/2>10){
+    printf("SIM! \n");
+  }else(a < 10);{
+    printf("NÃO! \n");
+
+  }
+
+
+  return 0;
+}
